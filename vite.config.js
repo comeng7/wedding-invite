850de91d2cda83ec03cb4627b743d7eb6f789e9e
정장오 매니저 <comeng7@hyundai.com>
2025-05-02 13:58:51 +0900

    feat: 이미지 링크 수정

--- vite.config.js
+++ vite.config.js
@@ -6,8 +6,8 @@ import { defineConfig } from 'vite';
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
-  base: '/wedding-invite/',
   resolve: {
     alias: [{ find: '@', replacement: path.resolve(__dirname, 'src') }],
   },
+  base: '/wedding-invite/',
 });
