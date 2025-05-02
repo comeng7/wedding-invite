46599cf476c3f02462c524f6e317697f4c4ec66d
정장오 매니저 <comeng7@hyundai.com>
2025-05-02 13:34:51 +0900

    Updates

---
+++ .prettierrc.cjs
@@ -0,0 +1,12 @@
+// .prettierrc.cjs
+module.exports = {
+  printWidth: 100, // 한 줄 최대 길이
+  tabWidth: 2, // 탭 너비
+  useTabs: false, // 탭 대신 스페이스 사용
+  semi: true, // 세미콜론 사용
+  singleQuote: true, // 작은따옴표 사용
+  trailingComma: 'all', // 후행 콤마 사용 (es5, all, none)
+  bracketSpacing: true, // 객체 리터럴 괄호 사이에 공백 추가 { foo: bar }
+  arrowParens: 'always', // 화살표 함수 매개변수 괄호 항상 사용 (x) => x
+  endOfLine: 'lf', // 줄 바꿈 문자 LF
+};
